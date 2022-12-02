f23a853705b333348d24b54001afd9f5fcc36af0
649BC <neebotoo@ruru.be>
2022-12-02 09:51:04 +0700

    remove sample ifo

--- src/config/constants/ifo.ts
+++ src/config/constants/ifo.ts
@@ -6,22 +6,22 @@ import { Ifo } from './types'
 export const cakeBnbLpToken = new Token(ChainId.BSC, CAKE_BNB_LP_MAINNET, 18, 'WKD-BNB LP')
 
 const ifos: Ifo[] = [
-  {
-    id: 'peel',
-    address: '0x35Bb6Dd4E8C63491057c32621c8cDdE43BabE201',
-    isActive: false,
-    name: 'Meta Apes ($PEEL)',
-    symbol: 'PEEL',
-    token: bscTokens.peel,
-    decimal: 18,
-    fundsToRaise: '2 Bnb',
-    offeringAmount: '83333333333333328',
-    articleUrl:
-      'https://pancakeswap.finance/voting/proposal/bafkreibomj5nilvyckdro7ztmm62syt55dcfnonxs63ji6hm2ijq35lru4',
-    twitterUrl: 'https://twitter.com/MetaApesGame',
-    description:
-      'Meta Apes is a free-to-play, play-and-earn massively multiplayer online (MMO) strategy game designed for mobile and the first game to launch on the BNB Sidechain with their own dedicated chain, Ape Chain.',
-  },
+  // {
+  //   id: 'peel',
+  //   address: '0x35Bb6Dd4E8C63491057c32621c8cDdE43BabE201',
+  //   isActive: false,
+  //   name: 'Meta Apes ($PEEL)',
+  //   symbol: 'PEEL',
+  //   token: bscTokens.peel,
+  //   decimal: 18,
+  //   fundsToRaise: '2 Bnb',
+  //   offeringAmount: '83333333333333328',
+  //   articleUrl:
+  //     'https://pancakeswap.finance/voting/proposal/bafkreibomj5nilvyckdro7ztmm62syt55dcfnonxs63ji6hm2ijq35lru4',
+  //   twitterUrl: 'https://twitter.com/MetaApesGame',
+  //   description:
+  //     'Meta Apes is a free-to-play, play-and-earn massively multiplayer online (MMO) strategy game designed for mobile and the first game to launch on the BNB Sidechain with their own dedicated chain, Ape Chain.',
+  // },
   // {
   //   id: 'trivia',
   //   address: '0x23C520d8227524E2cDD00360358864fF3fFC36b4',
